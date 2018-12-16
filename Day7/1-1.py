fffc2a00e0566c34e0e392743a8c84cec1c9e314
Marija Nedjalkova <nedyalkova.marya@gmail.com>
2018-12-16 00:56:22 +0000

    cleanup after part 1 of Day 7

--- Day7/1-1.py
+++ Day7/1-1.py
@@ -1,6 +1,5 @@
 import re
 from operator import attrgetter
-
 from Day7.classes import Node
 
 
@@ -23,65 +22,27 @@ def extract_before_and_after_from_string(line):
     return word_map["before"], word_map["after"]
 
 
-def have_not_seen_before(before, after, prev):
-    return before not in prev and after not in prev
-
-
-# def find_in_flows(sought_value, node):
-#     if node is None:
-#         return None
-#     if node.value == sought_value:
-#         return node
-#     for child in node.children:
-#         found = find_in_flows(sought_value, child)
-#         if found:
-#             return found
-#
-#
-# def find_among_seen(value, flows):
-#     for flow in flows:
-#         found = find_in_flows(value, flows[flow])
-#         if found:
-#             return found
-
-
 def main():
     with open("input.txt") as task_input:
         content_array = task_input.read().split("\n")
         nodes = {}
         for line in content_array:
-            print(line)
             before_value, after_value = extract_before_and_after_from_string(line)
-            if have_not_seen_before(before_value, after_value, nodes):
-                after = Node(after_value)
-                before = Node(before_value)
-                before.add_child(after)
-                after.add_parent(before)
-                nodes[before_value] = before
-                nodes[after_value] = after
-            else:
-                before_node = nodes[before_value] if before_value in nodes else None
-                after_node = nodes[after_value] if after_value in nodes else None
-                if before_node and after_node:
-                    common_parents = set(before_node.parents).intersection(after_node.parents)
-                    for common_parent in common_parents:
-                        after_node.parents.remove(common_parent)
-                        common_parent.children.remove(after_node)
-                    after_node.add_parent(before_node)
-                    before_node.add_child(after_node)
-                elif after_node:
-                    # have not seen before but seen after
-                    before_node = Node(before_value)
-                    nodes[before_value] = before_node
-                    after_node.add_parent(before_node)
-                    before_node.add_child(after_node)
-                else:
-                    # seen only before_node
-                    after_node = Node(after_value)
-                    nodes[after_value] = after_node
-                    after_node.add_parent(before_node)
-                    before_node.add_child(after_node)
+            before_node = nodes[before_value] if before_value in nodes else create_node_put_in_map(before_value, nodes)
+            after_node = nodes[after_value] if after_value in nodes else create_node_put_in_map(after_value, nodes)
+            common_parents = set(before_node.parents).intersection(after_node.parents)
+            for common_parent in common_parents:
+                after_node.parents.remove(common_parent)
+                common_parent.children.remove(after_node)
+            before_node.add_child(after_node)
+            after_node.add_parent(before_node)
         print_steps(nodes)
+
+
+def create_node_put_in_map(value, nodes):
+    node = Node(value)
+    nodes[value] = node
+    return node
 
 
 def is_available(child, prev):
@@ -92,16 +53,15 @@ def print_steps(nodes):
     available = set([nodes[node] for node in nodes if len(nodes[node].parents) == 0])
     steps = []
     while(len(available)) > 0:
-        print("available", available)
         next_node = choose_next_from_available(available)
-        print(next_node.value)
         steps.append(next_node.value)
         for child in next_node.children:
-            # only add the child if all is parents are in steps
+            # only add the child if all is parents have executed
             if is_available(child, steps):
                 available.add(child)
         available.remove(next_node)
-    print("".join(steps))
+    answer = "".join(steps)
+    assert ("ACHOQRXSEKUGMYIWDZLNBFTJVP" == answer)
 
 
 def choose_next_from_available(nodes):
--- Day7/classes.py
+++ Day7/classes.py
@@ -1,51 +1,12 @@
 class Node:
 
-    def __init__(self, value, parent_node=None, child=None):
+    def __init__(self, value):
         self.value = value
         self.parents = []
-        if parent_node is not None:
-            self.add_parent(parent_node)
         self.children = []
-        if child is not None:
-            self.add_child(child)
 
     def add_child(self, child):
         self.children.append(child)
 
     def add_parent(self, parent):
         self.parents.append(parent)
-
-    def add_child_by_value(self, value):
-        self.children.append(Node(value, parent_node=self))
-
-    def add_parent_by_value(self, value):
-        self.parents.append(Node(value, child=self))
-
-    def __eq__(self, other):
-        if isinstance(other, Node):
-            return self.value == other.value
-            # if self.value != other.value:
-            #     return False
-            # if len(self.parents) != len(other.parents):
-            #     return False
-            # if len(self.children) != len(other.children):
-            #     return False
-            # return not (any([this_child.value != that_child.value for (this_child, that_child) in
-            #                  zip(self.children, other.children)])
-            #             or any([this_parent.value != that_parent.value for (this_parent, that_parent) in
-            #                     zip(self.parents, other.parents)]))
-        return False
-
-    def deep_equals(self, other):
-        return (self == other) and len(self.children) == len(other.children) and not any(
-            [this_child != that_child for (this_child, that_child) in zip(self.children, other.children)])
-
-    # def __repr__(self):
-    #     self.children.sort(key=lambda x: x.value)
-    #     return "[{}] => {}".format(self.value, self.children[0] if len(self.children) > 0 else "")
-
-    def __repr__(self):
-        return self.value
-
-    def __hash__(self):
-        return hash(self.value)
