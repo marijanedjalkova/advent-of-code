e65d7a813a8a72ea368290f782637502ddda371a
Marija Nedjalkova <nedyalkova.marya@gmail.com>
2018-12-18 23:53:02 +0000

    more additional methods

--- Day13/1-1.py
+++ Day13/1-1.py
@@ -9,8 +9,33 @@ def is_cart(char):
     return char in ["V", "^", "<", ">"]
 
 
-def get_road_of_direction(char):
-    return "-" if char in ["<", ">"] else "|"
+def get_neighbours(cart, road_map, horizontal=False):
+    if not horizontal:
+        return road_map[cart.x][cart.y + 1], road_map[cart.x][cart.y - 1]
+    return road_map[cart.x + 1][cart.y], road_map[cart.x - 1][cart.y]
+
+
+def is_on_diagonal(cart, road_map):
+    neighbours = get_neighbours(cart, road_map, cart in ["<", ">"])
+    return neighbours[0] != neighbours[1]
+
+
+def get_road_of_direction(cart, road_map):
+    switcher = {
+        "^": "\\" if road_map[cart.x + 1][cart.y] else "/",
+        "V": "/" if road_map[cart.x + 1][cart.y] else "\\",
+        "<": "\\" if road_map[cart.x][cart.y + 1] else "/",
+        ">": "/" if road_map[cart.x][cart.y + 1] else "\\"
+    }
+    if is_on_diagonal(cart, road_map):
+        return switcher[cart.direction]
+    else:
+        return "-" if cart.direction in ["<", ">"] else "|"
+
+
+def tick(road_map, carts):
+    for cart in carts:
+        pass
 
 
 def read_in_input(filename):
@@ -24,18 +49,24 @@ def read_in_input(filename):
             for char in line:
                 if row not in road_map:
                     road_map[row] = {}
+                road_map[row][col] = None
                 if is_cart(char):
                     carts.append(Cart(row, col, char))
-                    road_map[row][col] = get_road_of_direction(char)
-                else:
+                elif char != " ":
                     road_map[row][col] = char
                 col += 1
             row += 1
     return road_map, carts
 
 
+def place_carts(road_map, carts):
+    for cart in carts:
+        road_map[cart.x][cart.y] = get_road_of_direction(cart, road_map)
+
+
 def main():
     road_map, carts = read_in_input("input.txt")
+    place_carts(road_map, carts)
     print(carts)
 
 
