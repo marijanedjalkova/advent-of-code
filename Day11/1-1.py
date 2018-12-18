9a1a0e88a344a8f047e9fd5f0de229820330c77e
Marija Nedjalkova <nedyalkova.marya@gmail.com>
2018-12-18 18:42:57 +0000

    solves part 2, massive improvement on efficiency. This solution uses the divide and conquer method

--- Day11/1-1.py
+++ Day11/1-1.py
@@ -1,4 +1,5 @@
-from Day11.classes import get_hundreds, Cell, get_power_level
+from Day11.classes import get_hundreds, get_power_level
+import math
 
 
 def tests():
@@ -7,64 +8,49 @@ def tests():
     assert 0 == get_hundreds(-99)
     assert 1 == get_hundreds(-100)
     assert 2 == get_hundreds(234)
-    assert 4 == Cell(3, 5, 8).get_power_level()
-    assert -5 == Cell(122, 79, 57).get_power_level()
-    assert 0 == Cell(217, 196, 39).get_power_level()
-    assert 4 == Cell(101, 153, 71).get_power_level()
-    assert 4 == get_total_power([Cell(3, 5, 8)])
-    assert 3 == get_total_power([Cell(3, 5, 8), Cell(122, 79, 57),
-                                 Cell(217, 196, 39), Cell(101, 153, 71)])
+    assert 4 == get_power_level(3, 5, 8)
+    assert -5 == get_power_level(122, 79, 57)
+    assert 0 == get_power_level(217, 196, 39)
+    assert 4 == get_power_level(101, 153, 71)
 
 
-def get_total_power(cells):
-    return sum([cell.power for cell in cells])
-
-
-def add_cell(x, y, existing_cells, existing_square_cells):
-    if not (x, y) in existing_cells:
-        new_cell = Cell(x, y)
-        existing_cells[(x, y)] = new_cell
-    existing_square_cells.append(existing_cells[(x, y)])
-
-
-def get_square_at(x, y, existing_cells, existing_squares, power_map, size=3):
-    if x in existing_squares and y in existing_squares[x]:
-        new_sum = power_map[x][y]
-        existing_square_cells = existing_squares[x][y]
-        for delta in range(size - 1):
-            add_cell(x + delta, y + size - 1, existing_cells, existing_square_cells)
-            new_sum += get_power_level(x + delta, y + size - 1)
-            add_cell(x + size - 1, y + delta, existing_cells, existing_square_cells)
-            new_sum += get_power_level(x + size - 1, y + delta)
-        add_cell(x + size - 1, y + size - 1, existing_cells, existing_square_cells)
-        new_sum += get_power_level(x + size - 1, y + size - 1)
-        power_map[x][y] = new_sum
-        return existing_square_cells, new_sum
-    existing_square_cells = []
+def get_square_at(x, y, power_map, size=3):
+    if x in power_map and y in power_map[x]:
+        if size % 2 == 0:
+            # combine the sums of 4 smaller squares
+            new_sum = power_map[x][y][size / 2] + power_map[x + size / 2][y][size / 2] + power_map[x][y + size / 2][size / 2] + power_map[x + size / 2][y + size / 2][size / 2]
+            power_map[x][y][size] = new_sum
+            return new_sum
+        else:
+            # say, 11. Then can take 6x6, 5x5, 5x5 and 6x6 and remove the central square
+            half_and_a_bit = math.ceil(size / 2)
+            almost_half = size - half_and_a_bit
+            sum_top_left = power_map[x][y][half_and_a_bit]
+            sum_top_right = power_map[x + half_and_a_bit][y][almost_half]
+            sum_bottom_left = power_map[x][y + half_and_a_bit][almost_half]
+            sum_bottom_right = power_map[x + almost_half][y + almost_half][half_and_a_bit]
+            central_square = power_map[x + almost_half][y + almost_half][1]
+            new_sum = sum_top_left + sum_top_right + sum_bottom_left + sum_bottom_right - central_square
+            power_map[x][y][size] = new_sum
+            return new_sum
     new_sum = 0
     for x_pos in range(x, x + size):
         for y_pos in range(y, y + size):
-            add_cell(x_pos, y_pos, existing_cells, existing_square_cells)
             new_sum += get_power_level(x_pos, y_pos)
-    if x not in existing_squares:
-        existing_squares[x] = {}
-    existing_squares[x][y] = existing_square_cells
     if x not in power_map:
         power_map[x] = {}
-    power_map[x][y] = new_sum
-    return existing_square_cells, new_sum
+    if y not in power_map[x]:
+        power_map[x][y] = {}
+    power_map[x][y][size] = new_sum
+    return new_sum
 
 
-def get_top_corner(max_square):
-    return min(max_square, key=lambda cell: cell.x * cell.y)
-
-
-def get_max_square_of_size(size, existing_cells, existing_squares, power_map):
+def get_max_square_of_size(size, power_map, board_size):
     max_power = 0
     max_square = None
-    for x in range(1, 301 - size + 1):
-        for y in range(1, 301 - size + 1):
-            square, power = get_square_at(x, y, existing_cells, existing_squares, power_map, size)
+    for x in range(1, board_size + 1 - size + 1):
+        for y in range(1, board_size + 1 - size + 1):
+            power = get_square_at(x, y, power_map, size)
             if power > max_power:
                 max_power = power
                 max_square = (x, y)
@@ -72,27 +58,23 @@ def get_max_square_of_size(size, existing_cells, existing_squares, power_map):
 
 
 def main():
-    existing_cells = {}
-    existing_squares = {}
     power_map = {}
-    top_corner, max_power = get_max_square_of_size(3, existing_cells, existing_squares, power_map)
+    top_corner, max_power = get_max_square_of_size(3, power_map, 300)
     print("Part 1 answer:", top_corner[0], top_corner[1])
     assert (21, 61) == (top_corner[0], top_corner[1])
-    existing_squares = {}
     power_map = {}
     max_power = 0
     top_corner = None
     max_size = 0
     for i in range(1, 301):
-        corner, power = get_max_square_of_size(i, existing_cells, existing_squares, power_map)
+        corner, power = get_max_square_of_size(i, power_map, 300)
         if power > max_power:
             max_power = power
             top_corner = corner
             max_size = i
     print("Answer for part2:", top_corner[0], ",", top_corner[1], ",", max_size)
     assert max_size == 12
-    assert top_corner[0] == 232
-    assert top_corner[1] == 251
+    assert (232, 251) == top_corner
 
 
 if __name__ == "__main__":
--- Day11/classes.py
+++ Day11/classes.py
@@ -1,27 +1,7 @@
-class Cell:
-
-    def __init__(self, x, y, serial_number=6042):
-        self.x = x  # 1 to 300, top left (1,1), top right (300,1)
-        self.y = y
-        self.serial_number = serial_number
-        self.power = self.get_power_level()
-
-    def get_power_level(self):
-        rack_id = self.x + 10
-        power_level = rack_id * self.y
-        power_level += self.serial_number
-        power_level *= rack_id
-        result = get_hundreds(power_level) - 5
-        return result
-
-    def __repr__(self):
-        return "({},{})".format(self.x, self.y)
-
-
-def get_power_level(x, y):
+def get_power_level(x, y, serial_number=6042):
     rack_id = x + 10
     power_level = rack_id * y
-    power_level += 6042
+    power_level += serial_number
     power_level *= rack_id
     result = get_hundreds(power_level) - 5
     return result
