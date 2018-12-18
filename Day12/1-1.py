a7fb6a1427afb9ea160fd0f9678ad7b4b6803c73
Marija Nedjalkova <nedyalkova.marya@gmail.com>
2018-12-18 19:06:22 +0000

    test input produces correct result

--- Day12/1-1.py
+++ Day12/1-1.py
@@ -2,9 +2,11 @@ from collections import deque
 
 
 def tests():
-    assert 0 == get_row_sum([])
-    assert 1 == get_row_sum([False, True, False])
-    assert 3 == get_row_sum([False, True, True])
+    assert 0 == get_row_sum([], 0)
+    assert 1 == get_row_sum([False, True, False], 0)
+    assert 3 == get_row_sum([False, True, True], 0)
+    assert -1 == get_row_sum([False, True, False], -2)
+    assert 7 == get_row_sum([False, True, True], 2)
     assert "." == row_to_str([False])
     assert "#" == row_to_str([True])
     assert ".#." == row_to_str([False, True, False])
@@ -12,11 +14,16 @@ def tests():
     assert deque([True]) == parse_str("#")
     assert deque([False]) == parse_str(".")
     task_input, rules = read_in_input("test-input.txt")
-    assert 325 == get_final_sum(20, task_input, rules)
+    result_sum = get_final_sum(20, task_input, rules)
+    assert 325 == result_sum
 
 
 def get_row_sum(row, left_most_coord):
-    return sum([tpl[0] for tpl in enumerate(row) if tpl[1]])
+    res = 0  # False, True, False , -2 but we want 0, 1, 2
+    for i in range(len(row)):
+        if row[i] is True:
+            res += (i + left_most_coord)
+    return res
 
 
 def take_five(row, i):
@@ -25,7 +32,6 @@ def take_five(row, i):
 
 def process_boolean_5(boolean_row, i, rules):
     five_values = take_five(boolean_row, i)
-    print(five_values)
     five = row_to_str(five_values)
     return rules[five] if five in rules else False
 
@@ -43,8 +49,8 @@ def get_next_gen(boolean_row, left_most, rules):
     """
     boolean_row.extendleft([False] * 4)
     boolean_row.extend([False] * 4)
-    left_most -= 4
     result_booleans = deque([process_boolean_5(boolean_row, i, rules) for i in range(2, len(boolean_row) - 2)])
+    left_most -= 2  # because we only actually processed 2 of each 4 pads
     empty_spaces = 0
     while result_booleans[0] is False:
         empty_spaces += 1
@@ -66,11 +72,11 @@ def row_to_str(row):
 def get_final_sum(num_generations, row, rules):
     boolean_row = parse_str(row)
     left_most_coord = 0
-    print("0", "".join(row))
+    print(0, 0, "".join(row))
     for i in range(num_generations):
         boolean_row, left_most_coord = get_next_gen(boolean_row, left_most_coord, rules)
-        print(i+1, row_to_str(boolean_row))
-    return get_row_sum(row, left_most_coord)
+        print(i+1, left_most_coord, row_to_str(boolean_row))
+    return get_row_sum(boolean_row, left_most_coord)
 
 
 def read_in_input(filename):
